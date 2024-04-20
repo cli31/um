78fee4dc431d6ca3d2bddade66d13bd3e7ccfed4
cli31 <licheng.040623@outlook.com>
2024-04-20 10:02:46 -0400

    Original UM

--- um.c
+++ um.c
@@ -12,14 +12,14 @@ int main(int argc, char *argv[])
         if (argc != 2) {
         fprintf(stderr, "Usage: ./um [file.um]\n");
         exit(EXIT_FAILURE);
-}
+        }
         FILE *fp = fopen(argv[1], "rb");
         if (!fp) {
         fprintf(stderr, 
                 "Error: Cannot open file '%s' for reading.\n", 
                 argv[argc - 1]);
         exit(EXIT_FAILURE);
-}
+        }
         /* template done, code starts here */
         /* it is an oop idea, code inside of this block is equivalent to 
         um.run() */
